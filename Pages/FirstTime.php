1703880e02211e6b061f404815dae468d07d6aaa
Tudor-The-programmer <93143436+Tudor-The-programmer@users.noreply.github.com>
2022-07-06 21:35:14 +0100

    quick change

--- Pages/FirstTime.php
+++ Pages/FirstTime.php
@@ -12,17 +12,23 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Welcome!</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat&family=Work+Sans&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="../Styles/FirstTIme.css">
 </head>
 
 <body>
     <div class="text-space">
-        <h1>
-            Glad you made it!
-        </h1>
-        <p>
-            Select what you want to do:
-        </p>
+        <div class="text-styles">
+            <h1>
+                Glad you made it!
+            </h1>
+            <p>
+                Select what you want to do:
+            </p>
+        </div>
+
     </div>
 
     <form action="./FirstTime.php" method="post" id='form'>
