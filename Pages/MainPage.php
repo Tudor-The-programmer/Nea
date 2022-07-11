8913a01057c644d5090531acf28e75debc1465cb
Tudor-The-programmer <93143436+Tudor-The-programmer@users.noreply.github.com>
2022-07-11 18:23:20 +0100

    Great progress!

--- Pages/MainPage.php
+++ Pages/MainPage.php
@@ -1,13 +1,66 @@
 <?php
+
 session_start();
 $subjects = $_SESSION['subjects'];
+
+class SubjectTemplate
+{
+    public $subject;
+    public $subjectFile;
+
+    public function __construct($subject, $subjectFile)
+    {
+        $this->subjectFile = $subjectFile;
+        $this->subject = $subject;
+    }
+
+    public function diplsyAllUnits()
+    {
+        $flag = true;
+        $counter = 0;
+        $unit = 1;
+        while ($flag) {
+            if (file_exists($this->subjectFile . '/Unit' . $unit . '.csv')) {
+                $counter += 1;
+                $unit += 1;
+            } else {
+                $flag = false;
+            }
+        }
+
+        $x = 1;
+        while ($x < $counter + 1) {
+            echo '<a>Unit' . $x . '</a>';
+            $x++;
+        }
+    }
+}
+
+function setUpUnits($subjects)
+{
+    foreach ($subjects as $subject) {
+        $sub = new  SubjectTemplate($subject, $_SERVER['DOCUMENT_ROOT'] . '/nea/Databases/' . $subject);
+        echo '<div id="' . $subject . '2' . '" class="invisible panel ' . $subject . '">';
+        print '<h3>' . $subject . '</h3>';
+        echo '<ul>';
+        print '<a>' . $sub->diplsyAllUnits() . '</a>';
+        echo '</ul>';
+        echo '</div>';
+    }
+}
+
+
 
 function displaySubjects($subjects)
 {
     foreach ($subjects as $subject) {
-        echo '<button classname =subject >' . $subject . '</a>';
+        echo '<button class=subject onclick="toggleActive(id)" id ="' . $subject . '">' . $subject . '</a>';
     }
 }
+
+
+
+
 
 
 ?>
@@ -20,24 +73,52 @@ function displaySubjects($subjects)
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
     <link rel="stylesheet" href="../Styles/MainPage.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat&family=Work+Sans&display=swap" rel="stylesheet">
+    <title>Glad You're Here!</title>
 </head>
 
 <body>
-    <h1 class="title">
+    <div class="title">
+        <h1>
+            <?php echo 'Hello ' . $_SESSION['uname'] . '!'; ?>
+        </h1>
+    </div>
 
-        <?php echo 'Hello ' . $_SESSION['uname'] . '!'; ?>
-    </h1>
-    <div class="container">
+    <div class="tab-container">
         <nav>
-            <button class="home">Home</a>
+            <button class="home active" onclick='toggleActive(id)' id="Home">Home</a>
                 <?php displaySubjects($subjects); ?>
         </nav>
     </div>
 
-</body>
+    <div class="contentainer">
+        <div class="text-area">
+            <div class="panel" id='Home2'>
+                <h3>
+                    Home:
+                </h3>
 
+                <ul>
+                    <a href=' '>Progress</a>
+                    <a href=' '>Most Worked On</a>
+                    <a href=' '>Best Units</a>
+
+                </ul>
+            </div>
+
+            <?php
+
+            setUpUnits($subjects);
+
+            ?>
+
+        </div>
+    </div>
+
+</body>
 <script src="../Scripts/MainPage.js"></script>
 
 </html>
---
+++ Scripts/MainPage.js
@@ -0,0 +1,29 @@
+function toggleActive(id) {
+  /*this is a work around to get the id of the current active tab
+    it has to be done as the getting the element by class name doesnt 
+    allow for stylings to be made
+*/
+  let currentId = document.querySelector(".active").id;
+  let buttonActive = document.getElementById(currentId);
+  let button = document.getElementById(id);
+
+  let nextPanel = id + "2";
+  let currentPanel = currentId + "2";
+
+  let buttonActivePanel = document.getElementById(currentPanel);
+  let buttonPanel = document.getElementById(nextPanel);
+
+  button.classList.toggle("active");
+  buttonActive.classList.toggle("active");
+
+  buttonActivePanel.classList.toggle("invisible");
+  buttonPanel.classList.toggle("invisible");
+}
+
+const div = document.getElementById('');
+
+div.addEventListener("mousemove", function (e) {
+  x = e.offsetX;
+  y = e.offsetY;
+  div.style.backgroundColor = "rgb(${x}, ${y}, ${x - y})";
+});
